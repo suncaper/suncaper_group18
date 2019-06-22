1901e8076ac98ac3ceb6ec0953c16905a4ded54b
Bolvvv <605725874@qq.com>
2019-06-22 16:54:19 +0800

    添加mybatis-generator插件，逆向生成实体类及dao

--- back/src/main/java/org/group18/back/BackApplication.java
+++ back/src/main/java/org/group18/back/BackApplication.java
@@ -1,11 +1,9 @@
 package org.group18.back;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.group18.mapper")
 public class BackApplication {
 
     public static void main(String[] args) {
--- back/src/main/java/org/group18/back/Service/TestServiceImpl.java
+++ back/src/main/java/org/group18/back/Service/TestServiceImpl.java
@@ -1,7 +1,10 @@
 package org.group18.back.Service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TestServiceImpl implements TestService {
