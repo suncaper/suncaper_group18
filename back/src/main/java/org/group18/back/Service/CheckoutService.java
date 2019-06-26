fb21a941a4a08728032bd7f0a25637aa5439c8a1
Yolanda-Wang <751942153@qq.com>
2019-06-26 20:34:58 +0800

    实现提交订单的前端，和部分后端逻辑

--- back/src/main/java/org/group18/back/Service/CheckoutService.java
+++ back/src/main/java/org/group18/back/Service/CheckoutService.java
@@ -8,11 +8,7 @@ import java.util.List;
 
 
 public interface CheckoutService {
-<<<<<<< HEAD:back/src/main/java/org/group18/back/Service/checkoutService.java
     public List<UserAddress> getUserAddress(String user_uid);
     public BigDecimal getUserBalance(String user_uid);
     public Integer getUserPoints(String user_uid);
-=======
-    List<UserAddress> getUserAddress(String user_uid);
->>>>>>> 3fb18dd0ce92fb84621a4cf35877a16e3c13c6c4:back/src/main/java/org/group18/back/Service/CheckoutService.java
 }
